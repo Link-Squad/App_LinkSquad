971c5255f5a1fd3e6f5303e9267c006281576c19
TL-Garcia <tomas.bl.garcia@gmail.com>
2020-10-30 20:47:29 +0100

    feat: Complete sketch of UserCard. Styles need to be revised

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
 	return (
 		<main className="">
             Home
-			<UserCard user={{username: 'josete', friends: 58, views: 401, id:324234234}} />
+			<UserCard user={{username: 'josete', friends: 58, views: 401, id:324234234, img:'https://cdn141.picsart.com/275574803034201.jpg'}} />
 		</main>
 	);
 };
--- src/components/userCard/UserCard.jsx
+++ src/components/userCard/UserCard.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './UserCard.scss';
+import "./UserCard.scss";
 
-const UserCard = ({user}) => {
+const UserCard = ({ user }) => {
 	return (
 		<div className="UserCard">
-			<img className="UserCard__avatar" src={user.img}/>
+			<img className="UserCard__avatar" src={user.img} />
 			<article className="UserCard__body">
-                <p className="UserCard__stats"><span>{user.views}</span> Views</p>
-                <p className="UserCard__stats"><span>{user.friends}</span> Friends</p>
-                <p>CLICKABLE TEXT</p>
+				<div className="UserCard__stats-wrapper">
+					<p className="UserCard__stats">
+						<span>{user.views}</span> Views
+					</p>
+					<p className="UserCard__stats">
+						<span>{user.friends}</span> Friends
+					</p>
+					<p>CLICKABLE TEXT</p>
+				</div>
 
-                <Link to={`/users/${user.id}`} className="Button Button--primary Button--fake">View my profile</Link>
-            </article>
+				<Link
+					to={`/users/${user.id}`}
+					className="Button Button--primary Button--fake UserCard__Link"
+				>
+					View my profile
+				</Link>
+			</article>
 		</div>
 	);
 };
