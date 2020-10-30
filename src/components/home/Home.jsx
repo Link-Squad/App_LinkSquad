cb95b521f7d367f7b6e836cda735ff387c937ea0
TL-Garcia <tomas.bl.garcia@gmail.com>
2020-10-31 00:12:11 +0100

    chore: Initialized PlayerOverview component. Missing social media icons

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PlayerOverview from '../playerOverview/PlayerOverview';
 import UserCard from '../userCard/UserCard';
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
 		<main className="">
             Home
 			<UserCard user={{username: 'josete', friends: 58, views: 401, id:324234234, img:'https://cdn141.picsart.com/275574803034201.jpg'}} />
+			<PlayerOverview player={{username: 'bo$$619', friends: 58, views: 401, social:{youtube: 'youtube.com'}, id:324234234, img:'https://www.choiceofgames.com/press-files/screenshots/deathless/ios/iTunesArtwork.png'}} />
 		</main>
 	);
 };
---
+++ src/components/playerOverview/PlayerOverview.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaYoutube } from "react-icons/fa";
+import "./PlayerOverview.scss";
+
+const PlayerOverview = ({ player }) => {
+	return (
+		<div className="PlayerOverview">
+			<img src={player.img} className="PlayerOverview__avatar" />
+			<article className="PlayerOverview__body">
+				<p className="title">{player.username}</p>
+				<div>
+					<Link to={player.social.youtube}>
+						<FaYoutube />
+					</Link>
+				</div>
+				<Link
+					to={`/user/${player.id}`}
+					className="Button Button--primary Button-fake"
+				>
+					View Profile
+				</Link>
+			</article>
+		</div>
+	);
+};
+
+export default PlayerOverview;
