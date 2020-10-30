f5c56fc9bcc2187ff6c011edbca711aa84424f91
TL-Garcia <tomas.bl.garcia@gmail.com>
2020-10-30 20:17:34 +0100

    refactor: Create Button--fake class to add display: inline-block

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import UserCard from '../userCard/UserCard';
 
 const Home = () => {
 	return (
 		<main className="">
             Home
+			<UserCard user={{username: 'josete', friends: 58, views: 401, id:324234234}} />
 		</main>
 	);
 };
---
+++ src/components/userCard/UserCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './UserCard.scss';
+
+const UserCard = ({user}) => {
+	return (
+		<div className="UserCard">
+			<img className="UserCard__avatar" src={user.img}/>
+			<article className="UserCard__body">
+                <p className="UserCard__stats"><span>{user.views}</span> Views</p>
+                <p className="UserCard__stats"><span>{user.friends}</span> Friends</p>
+                <p>CLICKABLE TEXT</p>
+
+                <Link to={`/users/${user.id}`} className="Button Button--primary Button--fake">View my profile</Link>
+            </article>
+		</div>
+	);
+};
+
+export default UserCard;
