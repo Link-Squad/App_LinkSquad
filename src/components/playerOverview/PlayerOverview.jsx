2840997aabe9e09bded3f3ba41b111bf325d73b0
TL-Garcia <tomas.bl.garcia@gmail.com>
2020-10-31 11:37:40 +0100

    feat: Complete PlayerOverview styling
    Also create button--fake & icon classes inside class.css

--- src/components/playerOverview/PlayerOverview.jsx
+++ src/components/playerOverview/PlayerOverview.jsx
@@ -1,3 +1,5 @@
+import { faDiscord, faTwitch, faTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
 import "./PlayerOverview.scss";
@@ -7,14 +9,18 @@ const PlayerOverview = ({ player }) => {
 		<div className="PlayerOverview card">
 			<img src={player.img} className="PlayerOverview__avatar" />
 			<article className="PlayerOverview__body">
-				<p className="title">{player.username}</p>
-				<div>
+				<p className="PlayerOverview__name title">{player.username}</p>
+				<div className="PlayerOverview__social">
 					<Link to={player.social.youtube}>
+						<FontAwesomeIcon icon={faTwitter} className="PlayerOverview__icon icon"/>
+						<FontAwesomeIcon icon={faDiscord} className="PlayerOverview__icon icon"/>
+						<FontAwesomeIcon icon={faTwitch} className="PlayerOverview__icon icon"/>
+						<FontAwesomeIcon icon={faYoutube} className="PlayerOverview__icon icon"/>
 					</Link>
 				</div>
 				<Link
 					to={`/user/${player.id}`}
-					className="Button Button--primary Button-fake"
+					className="PlayerOverview__profile-link button--fake button--primary"
 				>
 					View Profile
 				</Link>
