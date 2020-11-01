6d9a510f637a0f85e11040d1e2fbe8610416d5b2
TL-Garcia <tomas.bl.garcia@gmail.com>
2020-11-01 23:37:29 +0100

    feat: Complete draft of Offer card

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,26 +6,21 @@ import PlayerCardSmall from '../playerCardSmall/PlayerCardSmall';
 import UserCard from '../userCard/UserCard';
 import './Home.scss';
 
-import {user1, user2} from '../../mocks/users.mock';
+import { user1, user2 } from '../../mocks/users.mock';
 import { overwatch } from '../../mocks/games.mock';
+import OfferCard from '../offerCard/OfferCard';
+import { offer1 } from '../../mocks/offers.mock';
 
 const Home = () => {
 	return (
 		<main className="Home">
 			Home
-			<UserCard
-				user={user2}
-			/>
+			<UserCard user={user2} />
 			<PlayerCardLong player={user1} />
-			<PlayerCardSmall
-				player={user1}
-			/>
-			<GameCardDetails
-				game={overwatch
-				}
-			/>
-
-			<PlayerCardDetails player={user1}/>
+			<PlayerCardSmall player={user1} />
+			<GameCardDetails game={overwatch} />
+			<PlayerCardDetails player={user1} />
+			<OfferCard offer={offer1} />
 		</main>
 	);
 };
