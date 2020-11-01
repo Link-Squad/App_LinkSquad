c4be13de4bbe1a6ce9fc9540fbf8646aac5c7fd6
TL-Garcia <tomas.bl.garcia@gmail.com>
2020-11-01 11:56:01 +0100

    fix: Change double quotes to single

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Landing from "./components/landing/Landing";
-import Header from "./components/header/Header";
-import Home from "./components/home/Home";
-import NotFound from "./components/notFound/NotFound";
-import Profile from "./components/profile/Profile";
-import "./App.scss";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Landing from './components/landing/Landing';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import NotFound from './components/notFound/NotFound';
+import Profile from './components/profile/Profile';
+import './App.scss';
 
 const App = () => {
 	return (
 		<div className="App">
 			<Header />
-      <div className='App__container' style={{backgroundImage: 'url(./background.png)'}}>
-        <div className="App__content">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Landing} />
-            <Route exact path="/profile" component={Profile} />
-            <Route component={NotFound} />
-          </Switch>
-        </div>
-      </div>
+			<div
+				className="App__container"
+				style={{ backgroundImage: 'url(./background.png)' }}
+			>
+				<div className="App__content">
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/login" component={Landing} />
+						<Route exact path="/profile" component={Profile} />
+						<Route component={NotFound} />
+					</Switch>
+				</div>
+			</div>
 		</div>
 	);
 };
