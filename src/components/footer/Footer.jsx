b59df81ea176b058d1cfa6e263d6cedd01058652
TL-Garcia <tomas.bl.garcia@gmail.com>
2020-11-02 14:31:30 +0100

    feat: Complete Footer styling. Currently it isn't conditionally rendered

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,24 +3,25 @@ import { Link } from 'react-router-dom';
 import SocialLinks from '../utilities/socialLinks/SocialLinks';
 import './Footer.scss';
 
-
 const Footer = () => {
 	return (
 		<footer className="Footer">
-			<nav className="Footer__nav">
-				<Link to="/info/conditions">Terms and conditions</Link>
-				<Link to="/info/legal">Legal</Link>
-				<Link to="/info/privacy">Privacy policy</Link>
-				<Link to="/info/cookies">Cookie policy</Link>
-				<Link to="/info/help">Help</Link>
-			</nav>
+			<div className="Footer__content">
+				<nav className="Footer__nav">
+					<Link to="/info/conditions">Terms and conditions</Link>
+					<Link to="/info/legal">Legal</Link>
+					<Link to="/info/privacy">Privacy policy</Link>
+					<Link to="/info/cookies">Cookie policy</Link>
+					<Link to="/info/help">Help</Link>
+				</nav>
 
-            <div>
-                <span>PIP Corporation &#169; 2020</span>
-                <SocialLinks social={{twitter:'twitter.com'}}/>
-            </div>
+				<div className="Footer__info">
+					<span>PIP Corporation &#169; 2020</span>
+					<SocialLinks social={{ twitter: 'twitter.com' }} />
+				</div>
+			</div>
 		</footer>
 	);
 };
 
-export default Footer
+export default Footer;
