1bc619e4ed16f8e98e11ea57c88487bcfcb3c574
TL Garcia <tomas.bl.garcia@gmail.com>
2020-11-03 12:24:34 +0100

    style: Update Header & children to make responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import NavBar from '../navBar/navbar';
+import NavBar from './navBar/navbar';
+import SearchBar from './searchBar/searchbar';
 import './Header.scss';
 
 const Header = () => {
 	return (
 		<header className="Header">
-			<NavBar />
+			<div className="Header__content">
+				<img src="logo.png" alt="linksquad logo" />
+				<SearchBar />
+				<NavBar />
+			</div>
 		</header>
 	);
 };
