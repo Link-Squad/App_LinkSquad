69326c9f301eeb1be7030b341daa827e293fa779
TL Garcia <tomas.bl.garcia@gmail.com>
2020-11-03 10:59:20 +0100

    style: Change absolute values to % or viewport to make more responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import NavBar from '../navBar/navbar'
+import React from 'react';
+import NavBar from '../navBar/navbar';
 import './Header.scss';
 
- const Header = () => {
-    return <header className="Header">
-        <NavBar/>
-    </header>
-}
+const Header = () => {
+	return (
+		<header className="Header">
+			<NavBar />
+		</header>
+	);
+};
 
 export default Header;
--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,22 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import LoginForm from "../loginForm/LoginForm";
-import "./Landing.scss";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Footer from '../footer/Footer';
+import LoginForm from '../loginForm/LoginForm';
+import './Landing.scss';
 
 const Landing = () => {
 	return (
 		<main className="Landing">
 			<nav className="Landing__nav">
-				<Link to="/login" className="Button Button--primary Button--fake small">Log in</Link>
-				<Link to="/signup" className="Button Button--alt Button--fake small">Sign up</Link>
+				<Link to="/login" className="Button Button--primary Button--fake small">
+					Log in
+				</Link>
+				<Link to="/signup" className="Button Button--alt Button--fake small">
+					Sign up
+				</Link>
 			</nav>
 			<section className="Landing__body">
 				<div className="Landing__form">
 					<LoginForm />
 				</div>
-				<div className="Landing__sidebar">
-					<img className="Landing__img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Controller.svg/1024px-Controller.svg.png" height="265" width="265" alt="controller logo"/>
-				</div>
+				<img
+					className="Landing__img"
+					src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Controller.svg/1024px-Controller.svg.png"
+					alt="controller logo"
+				/>
 			</section>
 		</main>
 	);
