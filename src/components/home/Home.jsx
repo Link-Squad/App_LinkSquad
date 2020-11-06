2c021deddbbc504843e0e1c22cd9018776688def
TL-Garcia <tomas.bl.garcia@gmail.com>
2020-11-06 22:20:39 +0100

    fix: Results view style

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import PlayerCardSmall from '../playerCardSmall/PlayerCardSmall';
 import UserCard from '../userCard/UserCard';
 import './Home.scss';
 
-import { user1, user2 } from '../../mocks/users.mock';
 import { useAuthContext } from '../../contexts/AuthContext';
 import News from '../news/News';
 import FooterSmall from '../utilities/footerSmall/FooterSmall';
