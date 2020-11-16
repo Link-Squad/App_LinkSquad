d69718a2cbdfdca82b92dc13f02c97cffdaddafe
TL-Garcia <tomas.bl.garcia@gmail.com>
2020-11-16 11:08:10 +0100

    refactor: Give FooterSmall its own stylesheet

--- src/components/utilities/footerSmall/FooterSmall.jsx
+++ src/components/utilities/footerSmall/FooterSmall.jsx
@@ -3,12 +3,12 @@ import {Link} from 'react-router-dom';
 import './FooterSmall.scss';
 
 const FooterSmall = () => (
-	<nav className="Results__left-side__nav">
-		<Link to="/info/conditions">Terms and conditions</Link>
-		<Link to="/info/legal">Legal</Link>
-		<Link to="/info/privacy">Privacy policy</Link>
-		<Link to="/info/cookies">Cookie policy</Link>
-		<Link to="/info/help">Help</Link>
+	<nav className="FooterSmall">
+		<Link to="/info/conditions" className="FooterSmall__link">Terms and conditions</Link>
+		<Link to="/info/legal" className="FooterSmall__link">Legal</Link>
+		<Link to="/info/privacy" className="FooterSmall__link">Privacy policy</Link>
+		<Link to="/info/cookies" className="FooterSmall__link">Cookie policy</Link>
+		<Link to="/info/help" className="FooterSmall__link">Help</Link>
 	</nav>
 );
 
