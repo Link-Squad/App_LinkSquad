e90281ac8972ba18254e963fa49ac72cf60514cd
TL-Garcia <tomas.bl.garcia@gmail.com>
2020-11-17 16:52:23 +0100

    fix: Change checkbox HTML id from name to value

--- src/components/utilities/checkbox/Checkbox.jsx
+++ src/components/utilities/checkbox/Checkbox.jsx
@@ -3,8 +3,8 @@ import './Checkbox.scss';
 
 const Checkbox = ({name, value, handleChange, text}) => (
     <div className="Checkbox">
-			<input type="checkbox" className="Checkbox__input" id={`Checkbox__input_${name}`} name={name} value={value} onChange={handleChange}></input>
-			<label htmlFor={`Checkbox__input_${name}`} className="Checkbox__label">{text}</label>
+			<input type="checkbox" className="Checkbox__input" id={`Checkbox__input_${value}`} name={name} value={value} onChange={handleChange}></input>
+			<label htmlFor={`Checkbox__input_${value}`} className="Checkbox__label">{text}</label>
     </div>
 )
 
