c072c4678a4497704c787b7ce24c86ee77630566
TL-Garcia <tomas.bl.garcia@gmail.com>
2021-01-20 16:33:53 +0100

    fix: Make Auth component responsive

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -6,41 +6,37 @@ import Button from '../utilities/button/Button';
 import './Auth.scss';
 
 const Auth = () => {
-	const [hasAccount, setHasAccount] = useState('true');
+	const [formToDisplay, setFormToDisplay] = useState('login');
 
 	const handleClick = e => {
 		const { value } = e.target;
-		setHasAccount(value);
+		setFormToDisplay(value);
 	};
 
 	return (
 		<Layout withFooter={true} withHeader={true}>
-				<main className="Auth form">
-					<section className="Auth__form">
-						{hasAccount === 'true' ? <LoginForm /> : <SignupForm />}
-					</section>
-					<section className="Auth__nav-wrapper">
-						<div className="Auth__nav-buttons">
-							<Button
-								text="Log In"
-								alt={hasAccount !== 'true'}
-								value={'true'}
-								handleClick={handleClick}
-							/>
-							<Button
-								text="Sign Up"
-								alt={hasAccount === 'true'}
-								value={'false'}
-								handleClick={handleClick}
-							/>
-						</div>
-						<img
-							className="Auth__img"
-							src="/img/controller.png"
-							alt="controller logo"
+			<main className="Auth form">
+				{formToDisplay === 'login' ? <LoginForm /> : <SignupForm />}
+					<div className="Auth__buttons">
+						<Button
+							text="Log In"
+							alt={formToDisplay !== 'login'}
+							value={'login'}
+							handleClick={handleClick}
 						/>
-					</section>
-				</main>
+						<Button
+							text="Sign Up"
+							alt={formToDisplay === 'login'}
+							value={'signup'}
+							handleClick={handleClick}
+						/>
+					</div>
+					<img
+						className="Auth__img"
+						src="/img/controller.png"
+						alt="controller logo"
+					/>
+			</main>
 		</Layout>
 	);
 };
