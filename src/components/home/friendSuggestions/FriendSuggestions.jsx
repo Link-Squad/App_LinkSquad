76b3b8ebfb7f4fa5318ef22ed111a05de39eca8e
TL-Garcia <tomas.bl.garcia@gmail.com>
2021-02-02 19:16:08 +0100

    feat: Dynamically change the amount of friend suggestions based on window size

--- src/components/home/friendSuggestions/FriendSuggestions.jsx
+++ src/components/home/friendSuggestions/FriendSuggestions.jsx
@@ -1,6 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useLayoutEffect, useRef } from 'react';
 import { useAuthContext } from '../../../contexts/AuthContext';
-import { getRandomElementFromArray, removeUserFromArray } from '../../../helpers/helpers';
+import {
+	getRandomElementFromArray,
+	removeUserFromArray
+} from '../../../helpers/helpers';
 import { getUsersByQuery, getUsersByGame } from '../../../services/api.service';
 import UserCardSmall from '../../userCardSmall/UserCardSmall';
 import './FriendSuggestions.scss';
@@ -8,18 +11,38 @@ import './FriendSuggestions.scss';
 const FriendSuggestions = () => {
 	const { user } = useAuthContext();
 
-	const fetchedUsers = useRef([])
+	const fetchedUsers = useRef([]);
 	const [usersToDisplay, setUsersToDisplay] = useState();
-	const [userPagination, setUserPagination] = useState(0)
+	const [userPagination, setUserPagination] = useState(0);
+	const [usersAmount, setUsersAmount] = useState(3)
+
+	useLayoutEffect(() => {
+		const updateUsersAmount = () => {
+			const {innerWidth: width} = window
+
+			if (width < 620) {
+				setUsersAmount(1)
+			} else if (width > 620 && width < 900) {
+				setUsersAmount(2)
+			} else {
+				setUsersAmount(3)
+			}
+		};
+
+		window.addEventListener('resize', updateUsersAmount);
+		updateUsersAmount();
+
+		return () => window.removeEventListener('resize', updateUsersAmount);
+	}, []);
 
 	useEffect(() => {
-		const getUsersRequests = []
-		const {languages, userGames} = user;
+		const getUsersRequests = [];
+		const { languages, userGames } = user;
 
 		if (languages.length) {
 			const randomLanguage = getRandomElementFromArray(user.languages);
 			const languageQuery = `?languages=${randomLanguage}`;
-			const byLanguageRequest = getUsersByQuery(languageQuery)
+			const byLanguageRequest = getUsersByQuery(languageQuery);
 
 			getUsersRequests.push(byLanguageRequest);
 		}
@@ -28,44 +51,48 @@ const FriendSuggestions = () => {
 			const userGames = user.userGames?.map(ug => ug.game.id);
 			const randomGame = getRandomElementFromArray(userGames);
 			const byGameRequest = getUsersByGame(randomGame);
-			
+
 			getUsersRequests.push(byGameRequest);
 		}
 
 		if (!getUsersRequests.length) {
 			const allUsersRequest = getUsersByQuery('');
 
-			getUsersRequests.push(allUsersRequest)
+			getUsersRequests.push(allUsersRequest);
 		}
-		
 
-		Promise.all(getUsersRequests).then(
-			(response) => {
-				const users = removeUserFromArray(response.flat(), user.id)
+		Promise.all(getUsersRequests).then(response => {
+			const users = removeUserFromArray(response.flat(), user.id);
 
-				fetchedUsers.current = users
-				setUsersToDisplay(users.slice(0, 3))
-			}
-		);
+			fetchedUsers.current = users;
+			setUsersToDisplay(users.slice(0, usersAmount));
+		});
 	}, [user]);
 
 	useEffect(() => {
-		const sliceStart = userPagination * 3
-		const sliceEnd = sliceStart + 3
+		const sliceStart = userPagination * usersAmount;
+		const sliceEnd = sliceStart + usersAmount;
 
-		setUsersToDisplay(fetchedUsers.current.slice(sliceStart, sliceEnd))
-	}, [userPagination])
+		setUsersToDisplay(fetchedUsers.current.slice(sliceStart, sliceEnd));
+	}, [userPagination, usersAmount]);
 
-	const handleClick = (e) => {
+	const handleClick = e => {
 		setUserPagination(prev => {
-			if (prev * 3 + 3 >= fetchedUsers.current.length) return 0
-			return ++prev
-		})
-	}
+			if (
+				prev * usersAmount.current +
+					usersAmount.current >=
+				fetchedUsers.current.length
+			)
+				return 0;
+			return ++prev;
+		});
+	};
 
 	return (
 		<section className="FriendSuggestions">
-			<h2 className="FriendSuggestions__title title">Friend Suggestions</h2>
+			<h2 className="FriendSuggestions__title title">
+				Friend Suggestions
+			</h2>
 			<div className="FriendSuggestions__container">
 				<div className="FriendSuggestions__list">
 					{usersToDisplay?.map(suggestedUser => (
